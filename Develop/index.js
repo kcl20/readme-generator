dbc506a78160b9c5818de1da091ad9a60b6592cc
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-12-11 19:42:35 -0500

    Copied starter files.

---
+++ Develop/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the name of your app?",
+    "What is the description of your app?",
+    "How does one install your app?",
+    "How does one use your app?",
+    "How does one contribute to your app?",
+    "How does one report issues with your app?",
+    "How does one test your app?",
+    "What is the license for your app?",
+    "What is your GitHub username?",
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
